3113867ab72dbee7275cf837a8a37384fc263fc0
NoeAnsen <106491983+NoeAnsen@users.noreply.github.com>
2022-06-25 12:41:16 -0300

    Minor changes

--- cypress/e2e/hotel.feature
+++ cypress/e2e/hotel.feature
@@ -2,8 +2,8 @@ Feature: Hotel Page
 
 
 Scenario: Go to booking
-Given a User with App URL
-When Go to the booking part
+Given A User with the App URL
+When Go to the Reserve part
 Then Can access that part
 
 
--- cypress/e2e/hotel.ts
+++ cypress/e2e/hotel.ts
@@ -1,10 +1,10 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 
-Given("a User with App URL", () => {
+Given("A User with the App URL", () => {
     cy.visit('https://hotel.testplanisphere.dev/en-US/');
   });
-When("Go to the booking part", () => {
+When("Go to the Reserve part", () => {
     cy.get('[href="./plans.html"]').click();
   });
 Then("Can access that part", () => {
